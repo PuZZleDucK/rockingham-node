be52a21fd8182bb085e61ca01f10775432e9efeb
PuZZleDucK <puzzleduck@gmail.com>
2017-01-02 08:20:05 +1100

    first db add test working

--- scraper.js
+++ scraper.js
@@ -1,2 +1,8 @@
+var tools = require("tools");
 
 console.log("NodeJS scraper");
+
+// Url: http://www.rockingham.wa.gov.au/Services/Town-planning-services/Town-planning-advertising#Submissions
+
+// Fields: council_reference, address, description, info_url, comment_url, date_scraped
+// Bonus: date_received, on_notice_from, on_notice_to
--- test.js
+++ test.js
@@ -1,2 +1,16 @@
+var tools = require("./tools.js");
 
+console.log("NodeJS tools: " + tools);
 console.log("NodeJS tests");
+
+
+
+console.log("Test DB write");
+tools.write_record_with_values(["council_reference", "address", "description", "info_url", "comment_url", "date_scraped", "date_received", "on_notice_from", "on_notice_to"]);
+
+
+
+
+
+
+console.log('test record written');
