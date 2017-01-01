c951c6274798f0647b525473c898a69b9f90978f
PuZZleDucK <puzzleduck@gmail.com>
2017-01-02 10:03:09 +1100

    add delete table and row tests

--- test.js
+++ test.js
@@ -1,16 +1,28 @@
 var tools = require("./tools.js");
+var async = require('async');
 
-console.log("NodeJS tools: " + tools);
 console.log("NodeJS tests");
 
-
-
 console.log("Test DB write");
-tools.write_record_with_values(["council_reference", "address", "description", "info_url", "comment_url", "date_scraped", "date_received", "on_notice_from", "on_notice_to"]);
+tools.write_record_with_values(["council_reference", "address", "description", "info_url", "comment_url", "11-10-1979", "11-10-1979", "11-10-1979", "11-10-1979"]);
+tools.write_record_with_values(["council_reference2", "address2", "description2", "info_url2", "comment_url2", , "11-10-1979", "11-10-1979", "11-10-1979", "11-10-1979"]);
+console.log('  test record written');
+
+setTimeout(function() {
+    console.log('...wait');
+
+    console.log("Test ROW delete");
+    tools.delete_record_with_reference("council_reference2");
+    console.log('  test record Deleted');
+
+    setTimeout(function() {
+      console.log('...wait2');
+      console.log("Test DB delete");
+      tools.delete_data_table();
+      console.log('  test db Deleted');
+  }, 6000);
+
+}, 3000);
 
 
-
-
-
-
-console.log('test record written');
+console.log("NodeJS tests complete");
--- tools.js
+++ tools.js
@@ -3,19 +3,34 @@ var sqlite3 = require("sqlite3");
 module.exports = {
 
   write_record_with_values: function(values){
-    console.log("Write: " + values[0]);
+    console.log("   Write: " + values[0]);
     var db = new sqlite3.Database("data.sqlite");
     db.serialize(function() {
-      // Create new table
       db.run("CREATE TABLE IF NOT EXISTS data (council_reference TEXT, address TEXT, description TEXT, info_url TEXT, comment_url TEXT, date_scraped DATE, date_received DATE, on_notice_from DATE, on_notice_to DATE)");
-      // Insert a new record
       var statement = db.prepare("INSERT INTO data(council_reference, address, description, info_url, comment_url, date_scraped, date_received, on_notice_from, on_notice_to) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
-      statement.run("council_reference", "address", "description", "info_url", "comment_url", "11-10-1979", "11-10-1979", "11-10-1979", "11-10-1979");
+      statement.run(values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7], values[8]);
       statement.finalize();
     });
-    console.log("Write complete");
+    console.log("   Write complete");
+    return true;
+  },
+
+  delete_data_table: function(){
+    console.log("   Delete table");
+    var db = new sqlite3.Database("data.sqlite");
+    db.run("DROP TABLE 'data'");
+    db.close();
+    console.log("   Delete complete");
+    return true;
+  },
+
+  delete_record_with_reference: function(reference){
+    console.log("   Delete: " + reference);
+    var db = new sqlite3.Database("data.sqlite");
+    db.run("DELETE FROM data WHERE council_reference = '" + reference + "';");
+    db.close();
+    console.log("   Delete complete");
     return true;
   }
-
 
 };
